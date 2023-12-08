f969cd7e2bf1d4d342efc49ed6a19faaa26719db
sanyomor-01 <sanyomormichael@gmail.com>
2023-12-08 11:22:49 +0000

    player added and new card functionality modified

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-let firstCard = getRandomNumber()
-let secondCard = getRandomNumber()
-let sum = firstCard + secondCard
-const cards = [firstCard, secondCard]
+let sum = 0
+let cards = []
 let hasBlackJack = false
 let message = ""
-
-let isAlive = true
+let isAlive = false
 let  messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+/* Player object*/
+
+let player = {
+    name : "Nadia",
+    chips : 145
+}
+
+let playerEl = document.getElementById('player-el')
+playerEl.textContent = player.name + ":  $" + player.chips 
+
+
 /* Get random card number*/
 function getRandomNumber() {
-    return Math.floor( Math.random() *13) +1
+    let randomNuber = Math.floor( Math.random() *13) +1
+    
+    if(randomNuber ===1){
+        return 11
+    }
+    else if(randomNuber > 10) {
+        return 10
+    }
+    else{
+        return randomNuber
+    }
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomNumber()
+    let secondCard = getRandomNumber()
+
+    cards = [firstCard, secondCard]
+    sum = cards[0] + cards[1]
     renderGame()
 }
 
@@ -42,8 +66,11 @@ const  renderGame = () =>{
 }
 
 function newCard() {
-    let card = getRandomNumber()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if(isAlive ===true && hasBlackJack === false){
+        let card = getRandomNumber()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
+
